e8287f476949a0524c1f63f0a19c9d9ac0dfda19
Austin Denny <austinbdenny@gmail.com>
2020-04-10 13:31:05 -0500

    Added local storage.

--- src/index.js
+++ src/index.js
@@ -11,10 +11,55 @@ import rootReducer from './Reducers/rootReducer';
 import BaseLayout from './Components/layout/BaseLayout';
 import Container from './Components/Container';
 
+// let store = createStore(
+//   rootReducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// );
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <BaseLayout>
+//         <Switch>
+//           <Route exact path='/' component={App} />
+//           <Route exact path='/container' component={Container} />
+//         </Switch>
+//       </BaseLayout>
+//     </BrowserRouter>
+//   </Provider>,
+//   document.getElementById('root')
+// );
+
+let saveToLocalStorage = (state) => {
+  try {
+    const serializeState = JSON.stringify(state);
+    localStorage.setItem('state', serializeState);
+    //localStorage["state"] = serializeState
+  } catch (e) {
+    console.log(e);
+  }
+};
+let loadFromLocalStorage = () => {
+  try {
+    const serializeState = localStorage.getItem('state');
+    if (serializeState === null) {
+      return undefined;
+    } else {
+      return JSON.parse(serializeState);
+    }
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+};
+const persistedState = loadFromLocalStorage();
 let store = createStore(
   rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+store.subscribe(() => {
+  saveToLocalStorage(store.getState());
+});
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
